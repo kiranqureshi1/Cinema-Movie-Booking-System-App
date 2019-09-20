2ff050773a701d1afff33ded2df56fff271f7a35
kiranqureshi1 <kiranqureshioutloook.com>
2019-09-20 12:07:00 +0100

    file structure ready

--- CourseBookingSystemMain/Models/Movie.cs
+++ CourseBookingSystemMain/Models/Movie.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace WebApplication5.Models
 {
-    public class MovieContext : DbContext
-    {
+    //public class CustomerContext : DbContext
+    //{
 
+    //    public CustomerContext() : base("CustomerContext")
+    //    {
+    //    }
+
+    //    public DbSet<MembershipType> MembershipTypes { get; set; }
+    //    public DbSet<Customer> Customers { get; set; }
+    //}
+
+
+    public class Customer
+    {
+        public int id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public String Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        //public ICollection<Movie> Movie { get; set; }
+
+        //public MembershipType membershiptype { get; set; }
+        //public byte membershiptypeid { get; set; }
     }
 }
