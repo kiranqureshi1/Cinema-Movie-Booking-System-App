9e2f0b3a56fa2863c808194625e6076196eeeacf
kiranqureshi1 <kiranqureshioutloook.com>
2019-09-20 12:17:55 +0100

    customer file added

--- CourseBookingSystemMain/Models/Customer.cs
+++ CourseBookingSystemMain/Models/Customer.cs
@@ -7,8 +7,28 @@ using System.Web;
 
 namespace WebApplication5.Models
 {
-    public class CustomerContext : DbContext
-    {
+    //public class CustomerContext : DbContext
+    //{
 
+    //    public CustomerContext() : base("CustomerContext")
+    //    {
+    //    }
+
+    //    public DbSet<MembershipType> MembershipTypes { get; set; }
+    //    public DbSet<Customer> Customers { get; set; }
+    //}
+
+
+    public class Customer
+    {
+        public int id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public String Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public ICollection<Movie> Movie { get; set; }
+
+        //public MembershipType membershiptype { get; set; }
+        //public byte membershiptypeid { get; set; }
     }
 }
