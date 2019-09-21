f5f0bcbd3a189f97f6dbfe4bee3bf69f200e46ed
kiranqureshi1 <kiranqureshioutloook.com>
2019-09-21 10:46:43 +0100

    membershipType repo added

--- CourseBookingSystemMain/Repositories/MembershipTypeRepository/MembershipTypeRepository.cs
+++ CourseBookingSystemMain/Repositories/MembershipTypeRepository/MembershipTypeRepository.cs
@@ -1,11 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using WebApplication5.Models;
 
-namespace CourseBookingSystemMain.Repositories
+
+namespace WebApplication5.Repositories.CustomersRepositories
 {
-    public class MembershipTypeRepository
+    public class MembershipTypeRepository : IMembershipTypeRepository, IDisposable
     {
+        private DataContext context;
+
+        public MembershipTypeRepository(DataContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<MembershipType> GetMembershipTypes()
+        {
+            return context.MembershipTypes.ToList();
+        }
+        public MembershipType GetMembershipTypeByID(int Id)
+        {
+            return context.MembershipTypes.Find(Id);
+        }
+        public void InsertMembershipType(MembershipType MembershipType)
+        {
+            context.MembershipTypes.Add(MembershipType);
+        }
+        public void DeleteMembershipType(int Id)
+        {
+            MembershipType MembershipType = context.MembershipTypes.Find(Id);
+            context.MembershipTypes.Remove(MembershipType);
+        }
+        public void UpdateMembershipType(MembershipType MembershipType)
+        {
+            context.Entry(MembershipType).State = EntityState.Modified;
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
