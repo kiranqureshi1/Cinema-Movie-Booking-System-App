52a155bc242b2078e9cb99711300bbf335849858
kiranqureshi1 <kiranqureshioutloook.com>
2019-09-21 10:45:34 +0100

    customer repo added

--- CourseBookingSystemMain/Repositories/CustomerRepository/CustomeryRepository.cs
+++ CourseBookingSystemMain/Repositories/CustomerRepository/CustomeryRepository.cs
@@ -8,8 +8,57 @@ using WebApplication5.Models;
 
 namespace WebApplication5.Repositories.CustomersRepositories
 {
-    public class CustomerRepository
+    public class CustomerRepository : ICustomerRepository, IDisposable
     {
-        
+        private DataContext context;
+
+        public CustomerRepository(DataContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return context.Customers.ToList();
+        }
+        public Customer GetCustomerByID(int Id)
+        {
+            return context.Customers.Find(Id);
+        }
+        public void InsertCustomer(Customer customer)
+        {
+            context.Customers.Add(customer);
+        }
+        public void DeleteCustomer(int Id)
+        {
+            Customer customer = context.Customers.Find(Id);
+            context.Customers.Remove(customer);
+        }
+        public void UpdateCustomer(Customer customer)
+        {
+            context.Entry(customer).State = EntityState.Modified;
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
