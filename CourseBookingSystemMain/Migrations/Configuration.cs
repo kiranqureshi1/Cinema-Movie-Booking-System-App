fb8d8fa61b6605929fdcd4c6532e2c907c62a28b
kiranqureshi1 <kiranqureshioutloook.com>
2019-09-21 09:50:47 +0100

    migrations enabled

---
+++ CourseBookingSystemMain/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace CourseBookingSystemMain.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication5.Models.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebApplication5.Models.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- CourseBookingSystemMain/Models/DataContext.cs
+++ CourseBookingSystemMain/Models/DataContext.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
-namespace CourseBookingSystemMain.Models
+namespace WebApplication5.Models
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext() : base("DataContext")
+        {
+        }
+
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
     }
 }
