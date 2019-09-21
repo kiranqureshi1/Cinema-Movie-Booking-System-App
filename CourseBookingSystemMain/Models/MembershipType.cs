b435e02f9a0b68d97799d27f9b353cad5068dbfa
kiranqureshi1 <kiranqureshioutloook.com>
2019-09-21 09:43:54 +0100

    membershipType added

--- CourseBookingSystemMain/Models/MembershipType.cs
+++ CourseBookingSystemMain/Models/MembershipType.cs
@@ -6,8 +6,21 @@ using System.Web;
 
 namespace WebApplication5.Models
 {
-    public class MembershipTypeContext : DbContext
-    {
+    //public class MembershipTypeContext : DbContext
+    //{
+    //     public MembershipTypeContext() : base("CustomerContext")
+    //    {
+    //    }
+    //    public DbSet<MembershipType> MembershipTypes { get; set; }
+    //}
 
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignupFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+
+        public ICollection<Movie> Movie { get; set; }
     }
 }
